c51a4b3a19f7eef7fdafd362c9a2c17a8048b46e
shanmugapriya <shanmugapriya2072004@gmail.com>
2026-08-31 15:35:58 +0530

    Update production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,20 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((req) => {
   const user = localStorage.getItem('medivault_user');
+
   if (user) {
     const { token } = JSON.parse(user);
+
     if (token) {
       req.headers.Authorization = `Bearer ${token}`;
     }
   }
+
   return req;
 });
 
